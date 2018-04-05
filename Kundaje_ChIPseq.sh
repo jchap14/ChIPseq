95099419ef8f9a3872e69dda2e70c9fe09f8067e
James <jchap14@gmail.com>
2018-04-05 09:50:35 -0700

    fastq naming convention

--- Kundaje_ChIPseq.sh
+++ Kundaje_ChIPseq.sh
@@ -7,9 +7,9 @@
 source activate aquas_chipseq_py3
 
 ##### specify variables to pass to chipseq.py
-FASTQ1=$1
-NAME=`basename $FASTQ1 .trim.R1.fq.gz`
-FASTQ2=`echo $NAME.trim.R2.fq.gz`
+FASTQR1=$1
+NAME=`basename $FASTQR1 .trim.R1.fq.gz`
+FASTQR2=`echo $NAME.trim.R2.fq.gz`
 
 ## create tempscript
 cat > $NAME.tempscript.sh << EOF
@@ -23,7 +23,7 @@ source activate aquas_chipseq_py3
 
 python /srv/gsfs0/projects/snyder/chappell/TF_chipseq_pipeline/chipseq.py \
 -type histone --screen $NAME -pe -species hg19 -nth 12 \
--fastq1 $FASTQ1 -fastq2 $FASTQ2
+-fastq1_1 $FASTQR1 -fastq1_2 $FASTQR2
 
 ## deactivate conda environment
 EOF
